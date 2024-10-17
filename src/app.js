7bc19a629ac0e69747a85f0029e6d408eb2eabe8
001SwetaSharma <contact.swetasharma@gmail.com>
2024-10-17 22:31:21 +0530

    added routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = 9999;
+
 app.use((req,res) => {
-    console.log('Inside request handler...');
+    res.send('Inside request handler...');
+});
+
+app.get('/users', (req,res) => {
+    res.send({firstName: 'Sweta', lastName: 'Sharma', });
+});
+
+app.post('/users', (req,res) => {
+    res.send('Saved data successfully........');
+});
+
+app.use('/hello', (req,res) => {
+    res.send('hello hi');   
+});
+
+app.use('/hello/2', (req,res) => {
+    res.send('hello hello hello.............');
 });
 
 app.listen(port, () => {
     console.log(`Server successfully listening to port :: ${port}`);
-})
+});
